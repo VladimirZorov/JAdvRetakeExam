0a8515e6ff6425a48c83b3b0c0f526df9fc97453
Vladimir Zorov <vl.zorov@gmail.com>
2022-12-11 15:59:09 +0200

    Initial commit

---
+++ src/Exam22October2022/EnergyDrinks.java
@@ -0,0 +1,12 @@
+package Exam22October2022;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class EnergyDrinks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> sequenceCofeine = new ArrayDeque<>();
+    }
+}
