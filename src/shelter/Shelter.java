944c8faf9de07a7e4abd86c3a94653cb89ed262d
Vladimir Zorov <vl.zorov@gmail.com>
2022-12-12 18:47:24 +0200

    Add changes

--- src/shelter/Shelter.java
+++ src/shelter/Shelter.java
@@ -1,10 +1,47 @@
 package shelter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Shelter {
 
     public int capacity;
     public List<Animal> animals;
+
+    public Shelter(int capacity) {
+        this.capacity = capacity;
+        this.animals = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Animal> getAnimals() {
+        return this.animals;
+    }
+
+    public void setAnimals(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void add(Animal animal) {
+        if (this.animals.size() < this.capacity){
+            this.animals.add(animal);
+        }
+    }
+    public  boolean remove(String name) {
+        for (int i = 0; i < this.animals.size(); i++) {
+            if (this.animals.get(i).getName().equals(name)){
+                this.animals.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
